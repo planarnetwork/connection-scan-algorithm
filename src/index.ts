d2654467a034075345fcab7650c2daa635b6e7ac
Linus Norton <linusnorton@gmail.com>
2019-07-23 09:56:05 +0100

    update exports, bump version

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,7 @@ export * from "./gtfs/Gtfs";
 export * from "./journey/Connection";
 export * from "./journey/Journey";
 export * from "./journey/JourneyFactory";
+
+export * from "./query/DepartAfterQuery";
+export * from "./query/JourneyFilter";
+export * from "./query/MultipleCriteriaFilter";
