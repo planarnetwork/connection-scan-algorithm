f6dd9b8aaeb97385c5f9e9e88379089016fc2989
Linus Norton <linusnorton@gmail.com>
2020-06-10 13:15:44 +0100

    added test for interchange time

--- src/csa/ConnectionScanAlgorithm.spec.ts
+++ src/csa/ConnectionScanAlgorithm.spec.ts
@@ -112,6 +112,44 @@ describe("ConnectionScanAlgorithm", () => {
     chai.expect(journey.arrivalTime).to.equal(1055);
   });
 
+  /**
+   * In this scenario there are two trips running in parallel. Trip 1 arrives earliest at A, B and C and Trip 2 arrives
+   * earliest at D. It is not possible to change onto the second trip at C because of the interchange change, however
+   * the algorithm should detect that it was possible to board at A and add the connection. The list of connections
+   * will be incorrect as it will use trip 1 for A->B, B->C and then trip 2 for C->D. The results factory tidies this
+   * up by realising that the whole journey could be made on a single trip (trip 2).
+   */
+  it("checks for connections missed because of interchange time", () => {
+    const trip1 = [
+      c("A", "B", 1000, 1010, "1"),
+      c("B", "C", 1010, 1020, "1"),
+      c("C", "D", 1020, 1040, "1"),
+    ];
+
+    setStopTimes(trip1);
+
+    const trip2 = [
+      c("A", "B", 1005, 1015, "2"),
+      c("B", "C", 1015, 1025, "2"),
+      c("C", "D", 1025, 1035, "2"),
+    ];
+
+    setStopTimes(trip2);
+
+    const timetable = [...trip1, ...trip2].sort((a, b) => a.arrivalTime - b.arrivalTime);
+
+    const resultsFactory = new ScanResultsFactory({ "A": 10, "B": 10, "C": 10, "D": 10 });
+    const scanner = new ConnectionScanAlgorithm(timetable, noTransfers, resultsFactory);
+    const results = scanner.scan({ "A": 900 }, ["D"], 20200101, 0);
+    const [journey] = journeyResultsFactory.getJourneys(results, ["D"]);
+
+    chai.expect(journey.origin).to.equal("A");
+    chai.expect(journey.destination).to.equal("D");
+    chai.expect(journey.legs.length).to.equal(1);
+    chai.expect(journey.departureTime).to.equal(1005);
+    chai.expect(journey.arrivalTime).to.equal(1035);
+  });
+
 });
 
 export function setStopTimes(connections: TimetableConnection[]) {
